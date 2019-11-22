5b38f68245802e629c5e6657f04c99e4ddf3bb9c
Akshat Tekriwal <akshattekriwal123@gmail.com>
2019-11-22 09:12:21 -0500

    added os info

--- test.js
+++ test.js
@@ -1,15 +1,16 @@
 const express = require('express')
-const app = express()
+const app = express();
 const port = 3000
+const os = require("os");
 const faker = require("faker");
 
-console.log("hello akshat");
+console.log(`Type: ${os.type} | cpu: ${os.cpus()[0].model} | cores: ${os.cpus().length}
+Total Mem: ${os.totalmem()/1e+9} Gbs | Free Mem: ${os.freemem()/1e+9} Gbs \n` );
 
 
 class Person {
     constructor(){
-       this.firstName = `${faker.name.firstName()}`
-       this.lastName = `${faker.name.lastName()}`
+       this.date = `${faker.random.number()*faker.random.number()}`
     } 
 
 }
@@ -34,9 +35,18 @@ let result3 = Date.now()-time3;
 
 let time4 = Date.now(); 
 for(let i = 0 ; i < num;i++){
-    if(a.firstName === "amanda"){1+1}
+    if(a.firstName === "llllllllllllllll"){1+1}
 }
-let result4 = Date.now()-time;
+let result4 = Date.now()-time4;
+
+
+let b = []
+for(let i = 0 ; i < num;i++){
+    b.push(new Person);
+}
+let time5 = Date.now(); 
+b.sort((x,y)=> x.date > y.date);
+let result5 = Date.now()-time5;
 
 
 console.log(`push ${num} objects in array = ${result1}`)
@@ -46,6 +56,10 @@ console.log(`unshift object in array of size ${num}  = ${result2}`)
 console.log(`push object in array of size ${num}  = ${result3}`)
 
 console.log(`time taken to iterate an array of size ${num}  = ${result4}`)
+
+console.log(`time taken to sort an array of size ${num}  = ${result5}`)
+
+console.log( a[40000])
 
 
 
